fbf1924d0ee4edcca1e310a79a7f81dcbb8eadf6
Ismail Elshareef <ielshareef@edmunds.com>
2013-08-19 23:48:24 -0700

    Pushing SDK 0.1.6 and more code examples

--- edmunds.api.sdk.js
+++ edmunds.api.sdk.js
@@ -44,7 +44,7 @@ function EDMUNDSAPI(key) {
 	 * @private
 	 * @type string
 	 */
-	var _version = "0.1.5";
+	var _version = "0.1.6";
 	
 	/**
 	 * Assigned API Key. Register for an API Key <a href="http://developer.edmunds.com/apps/register">here</a>
@@ -62,7 +62,7 @@ function EDMUNDSAPI(key) {
 	 * @private
 	 * @type string
 	 */
-	var _base_url = "//api.edmunds.com";
+	var _base_url = window.location.protocol + "//api.edmunds.com";
 	
 	/**
 	 * The base URL for photos
@@ -71,7 +71,7 @@ function EDMUNDSAPI(key) {
 	 * @private
 	 * @type string
 	 */
-	var _base_media = "//media.ed.edmunds-media.com";
+	var _base_media = window.location.protocol + "//media.ed.edmunds-media.com";
 	
 	/**
 	 * The API response format
@@ -135,7 +135,6 @@ function EDMUNDSAPI(key) {
 		return _base_media;
 	};
 	
-	this.callbackId = 0;
 	/**
 	 * Make the API REST call
 	 *
@@ -149,28 +148,101 @@ function EDMUNDSAPI(key) {
         var queryString = _serializeParams(params),
             baseUrl = this.getBaseUrl();
         queryString = (queryString) ? '?' + queryString + '&api_key=' + _api_key + "&fmt=" + _response_format : '?api_key=' + _api_key + "&fmt=" + _response_format;
-    	var callbackfn = 'C' + (new Date().getTime());
-
-		var script = document.createElement('script');
-		script.src = baseUrl + method + queryString +'&callback='+callbackfn;
-		window[callbackfn] = function(data) {
-            successCallback(data);
-        };
-		script.onerror = function(e) {
-            abort(e);
-        };
-		
-		// Add the script asynchronously 
-		var s = document.getElementsByTagName('script')[0];
-		s.parentNode.insertBefore(script, s);
-		//return this.jsonp({
-       //    url: baseUrl + method + queryString,
-       //    timeout: 7000,
-       //    success: successCallback,
-       //    error: errorCallback,
-	   //	cache: true
-       //});
+        return this.jsonp({
+            url: baseUrl + method + queryString,
+            timeout: 7000,
+            success: successCallback,
+            error: errorCallback,
+			cache: true
+        });
 	};
+	
+	/**
+	 * Make the JSONP call
+	 *
+	 * @method jsonp
+	 * @param object options 
+	 *	{
+	 *		"url" : [the API call],
+	 *		"timeout": [time in milliseconds of how long to wait for the script to execute],
+	 *		"success": [the function to be invoked upon success],
+	 *		"error": [the function to be invoked upon error],
+	 *		"cache": [let the Mashery cache be or bust it. Boolean value with default is FALSE (keep the cache)],
+	 * 	}
+	 *						
+	 * @return {string} API REST call URL
+	 */
+	this.jsonp = (function(global) {
+	    'use strict';
+
+	    var callbackId = 0,
+	        documentHead = document.head || document.getElementsByTagName('head')[0];
+
+	    function createScript(url) {
+	        var script = document.createElement('script');
+	        script.setAttribute('type', 'text/javascript');
+	        script.setAttribute('async', true);
+	        script.setAttribute('src', url);
+	        return script;
+	    }
+
+	    return function(options) {
+	        options = options || {};
+
+	        var callbackName = 'callback' + (new Date().getTime())+ (++callbackId),
+	            url = options.url + '&callback=' + callbackName + (options.cache ? '' : '&_dc=' + new Date().getTime()),
+	            script = createScript(url),
+	            abortTimeout;
+
+	        function cleanup() {
+	            if (script) {
+	                script.parentNode.removeChild(script);
+	            }
+	            clearTimeout(abortTimeout);
+	            delete global[callbackName];
+	        }
+
+	        function success(data) {
+	            if (typeof options.success === 'function') {
+	                options.success(data);
+	            }
+	        }
+
+	        function error(errorType) {
+	            if (typeof options.error === 'function') {
+	                options.error(errorType);
+	            }
+	        }
+
+	        function abort(errorType) {
+	            cleanup();
+	            if (errorType === 'timeout') {
+	                global[callbackName] = function() {};
+	            }
+	            error(errorType);
+				window.console.log(errorType+": One of the scripts failed to load ("+callbackName+")");
+	        }
+
+	        global[callbackName] = function(data) {
+	            cleanup();
+	            success(data);
+	        };
+
+	        script.onerror = function(e) {
+	            abort(e);
+	        };
+
+	        documentHead.appendChild(script);
+
+	        if (options.timeout > 0) {
+	            abortTimeout = setTimeout(function() {
+	                abort('timeout');
+	            }, options.timeout);
+	        }
+
+	    };
+
+	}(window));
 }
 
 if (typeof window.sdkAsyncInit === "function") {
